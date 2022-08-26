b199d0e93ae1838fc3cf271cf7ec58fff1f37b59
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 20:36:16 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -69,6 +69,7 @@ function HeaderVariantA(props) {
 
 function HeaderVariantB(props) {
     const { title, isTitleVisible, logo, primaryLinks = [], secondaryLinks = [] } = props;
+    blar();
     return (
         <div className="flex items-center relative">
             <SiteLogoLink title={title} isTitleVisible={isTitleVisible} logo={logo} />

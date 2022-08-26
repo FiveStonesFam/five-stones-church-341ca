bbce825d71519235d3d1ccafbd7f640bbc362a72
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 20:25:53 +0000

    link-utils.js: changed

--- src/utils/link-utils.js
+++ src/utils/link-utils.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 function blar() {
-    
+
 }
 
 module.exports = {
-    urlPathFromFilePath,
+    blar,
     cssClassesFromUrlPath,
     cssClassesFromFilePath,
     flattenMarkdownData,

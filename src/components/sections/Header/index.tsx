b45e30269d1309cb2f3b74100dee5d261d7ed50f
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 20:28:18 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -78,7 +78,6 @@ function HeaderVariantB(props) {
                     data-sb-field-path=".primaryLinks"
                 >
                     <ListOfLinks links={primaryLinks} inMobileMenu={false} />
-                    hey
                 </ul>
             )}
             {secondaryLinks.length > 0 && (

ca9664a023fc87cd6965a25a7327864ff08ddad8
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 19:31:40 +0000

    index.tsx: changed

--- src/components/atoms/Link/index.tsx
+++ src/components/atoms/Link/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import NextLink from 'next/link';
-
+import { menu } from 
 export default function Link({ children, href, ...other }) {
     // Pass Any internal link to Next.js Link, for anything else, use <a> tag
     const internal = /^\/(?!\/)/.test(href);

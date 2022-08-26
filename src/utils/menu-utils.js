9e5322274fa8db81394baa8ccaecdaf4799c9a7a
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 20:48:49 +0000

    menu-utils.js: changed

--- src/utils/menu-utils.js
+++ src/utils/menu-utils.js
@@ -3,7 +3,7 @@ const path = require('path');
 function jankMenu() {
     const aboutLink = document.querySelector('[href="/about/"]')
     const element = document.createElement('div');
-          element.classList.add('blar')
+          element.classList.add('blar');
     console.log(aboutLink)
 
 }

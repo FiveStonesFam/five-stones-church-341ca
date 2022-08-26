8cb4c97d8d6396bf9c71a9e2cf1978dda144fb7a
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 23:40:18 +0000

    menu-utils.js: changed

--- src/utils/menu-utils.js
+++ src/utils/menu-utils.js
@@ -5,7 +5,7 @@ function jankMenu() {
     const element = document.createElement('div');
           element.classList.add('blar');
           element.textContent = "Baptism"
-          element.href = "/next"
+          element.href = "/next-steps/"
     aboutLink.appendChild(element)
 
 }

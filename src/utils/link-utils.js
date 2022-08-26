ed6070c079d380ced816e1577516255d8574e335
Stackbit Code Editor <projects@stackbit.com>
2022-08-26 20:41:00 +0000

    link-utils.js: changed

--- src/utils/link-utils.js
+++ src/utils/link-utils.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 function jankMenu() {
     const aboutLink = document.querySelector('[href="/about/"]')
-    console.log( aboutLink )
+    console.log(aboutLink)
 }
 
 module.exports = {
